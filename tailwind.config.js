37332101c29b1ee219dc36e5d634f162ec056482
mouasit <mustaphaouassit60@gmail.com>
2023-04-06 17:24:18 +0000

    add: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,15 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        blue: "#0a317b",
-        red: "#f65351",
+        blue: "hsl(219, 85%, 26%)",
+        red: "hsl(1, 90%, 64%)",
+        white: "hsl(0, 0%, 100%)",
+        veryLightGrayishBlue: "hsl(210, 60%, 98%)",
+        lightGrayishBlueOne: "hsl(211, 68%, 94%)",
+        lightGrayishBlueTwo: "hsl(205, 33%, 90%)",
+        grayishBlue: "hsl(219, 14%, 63%)",
+        darkGrayishBlue: "hsl(219, 12%, 42%)",
+        veryDarkBlue: "hsl(224, 21%, 14%)",
       },
       fontFamily: {
         jakarta: ["Plus Jakarta Sans"],
