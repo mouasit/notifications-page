95504adf7f6d397766823eb7366dffb80611ac29
mouasit <mustaphaouassit60@gmail.com>
2023-04-07 17:06:47 +0000

    add level heading

--- src/Components/Notifications.js
+++ src/Components/Notifications.js
@@ -7,6 +7,7 @@ import data from "../data";
 export default function Notifications() {
   return (
     <main className="flex flex-col gap-8 py-5 px-3 md:shadow-lg md:rounded-2xl md:py-8 md:px-8 md:my-8 bg-white max-w-[740px]">
+      <h1 className="absolute z-[-10]">Notification page</h1>
       <Header />
       <div className="flex flex-col gap-4">
         {data.map((e, index) => {
