5ee81ce0934c346a95b6daf53f02b85244ec3241
mouasit <mustaphaouassit60@gmail.com>
2023-04-07 18:22:11 +0000

    change span in CardComment

--- src/Components/CardComment.js
+++ src/Components/CardComment.js
@@ -5,15 +5,15 @@ export default function CardComment(props) {
     <button className="flex gap-3 items-start px-5 py-5 rounded-md">
       <img src={props.data.avatar} alt="user" className="w-11 h-11" />
       <span className="flex items-center justify-between w-full">
-        <div className="flex flex-col items-start">
-          <p className="text-left text-darkGrayishBlue">
+        <span className="flex flex-col items-start">
+          <span className="text-left text-darkGrayishBlue">
             <span className="capitalize font-bold text-veryDarkBlue mr-[.4rem] hover:text-blue">
               {props.data.username}
             </span>
             commented on your picture
-          </p>
+          </span>
           <span className="text-grayishBlue text-sm">{props.data.time}</span>
-        </div>
+        </span>
         <img
           src={props.data.avatarCommented}
           alt="user commented"
